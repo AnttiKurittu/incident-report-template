7a223462f7a3a09ae314dd31df42d648e79840ee
Antti Kurittu <antti@kurittu.org>
2025-01-02 12:15:09 +0200

    Rendered example report

--- render.sh
+++ render.sh
@@ -6,6 +6,3 @@ pandoc ./Report/*.md -o ${1:-./output/report.html} --css=./css/style-light.css -
 file_hash=$(sha256sum "${1:-./output/report.html}" | cut -d ' ' -f 1)
 echo "$current_date Document rendered to ${1:-./output/report.html} with file checksum $file_hash" >> render.log
 echo "Wrote the report to ${1:-./output/report.html}."
-
-
-
