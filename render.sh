63f207c81f7c5cc2b36f5c99817fde0fb71435fe
Antti Kurittu <antti@kurittu.org>
2025-01-02 08:55:09 +0200

    Added verbosity to render script

--- render.sh
+++ render.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+echo "Running Pandoc and processing attachments..."
 python3 process_attachments.py
 pandoc ./Report/*.md -o ./Output/report.docx --reference-doc=./Template/template.dotx --lua-filter=./Filters/pagebreak.lua --lua-filter=./Filters/blockquote.lua
+echo "Done."
